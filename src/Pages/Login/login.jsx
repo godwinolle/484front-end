aff9f791e2e1513d5c62a4845dbb35a9bc18b75e
Bradley Agyei <Bradley-Agyei@users.noreply.github.com>
2020-10-19 23:17:20 -0400

    Implemented login page

--- src/Pages/Login/login.jsx
+++ src/Pages/Login/login.jsx
@@ -1,33 +1,41 @@
 import React from 'react';
-
 import { Link } from 'react-router-dom' //importing 
+
+//importing components to use in other places
+import LogoSide from '../../components/logoSide/logoSide';
+
+//Icons for login page
+import { MdPerson } from 'react-icons/md'
+import { BiLockAlt } from 'react-icons/bi'
 
 import './login.css';
 
 const Login = () => {
   return (
-    <div className="">
-      <h2>Login</h2>
-      <h3>WELCOME BACK</h3>
+    <div className="login">
+      <div className="sideA">
+        <h2>Login</h2>
+        <h3>WELCOME BACK!</h3>
+        <form>
+          <label><span><MdPerson /></span>username </label>
+          <input type="text" />  <br /> <br />
 
-      <form>
-        <label>username </label>
-        <input type="text" />  <br /> <br />
-
-        <label>password </label>
-        <input type="password" /> <br/>
-
-        <p>forget password</p>
-
-        <button type="submit">Login &gt; </button>        
-      </form>
-
-    <br/>
-
-    <Link to="/signup">
-      <button>Create a new account &gt;</button>
-    </Link>
-
+          <label><span><BiLockAlt /></span>password </label>
+          <input type="password" /> <br/>
+          
+          <p className="loginForgotPassword">FORGOT PASSWORD</p> 
+          <button type="submit">LOGIN &gt; </button>        
+        </form>
+        <br/>
+        <div className="createAccount">
+          <Link to="/signup">
+            <button>CREATE A NEW ACCOUNT &gt;</button>
+          </Link>
+        </div>  
+      </div>
+        <div className="sideB">
+          <LogoSide />
+        </div>
     </div>
   );
 }
