94904647d7fbc97eae42aebe8d40f74ce257f0a2
Bradley Agyei <Bradley-Agyei@users.noreply.github.com>
2020-11-15 14:16:42 -0500

    Added hooks to login page

--- src/Pages/Login/login.jsx
+++ src/Pages/Login/login.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Link } from 'react-router-dom' //importing 
+import React, { useState } from 'react';
+import { Link, Redirect } from 'react-router-dom' //importing 
 
 //importing components to use in other places
 import LogoSide from '../../components/logoSide/logoSide';
@@ -11,18 +11,48 @@ import { BiLockAlt } from 'react-icons/bi'
 import './login.css';
 
 const Login = () => {
+
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState(''); //generates error message
+  const [redirect, setRedirect] = useState(false); //will redirect to feed page
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    username.trim();
+    password.trim();
+    if(username !== '' && password !== ''){
+      console.log(username, password)
+      setError('');
+      setRedirect(true);
+    } else{
+      setError('Invalid username and password combination entered')
+    }
+  }
+
+  if(redirect){
+    return <Redirect to="/feed" />
+  }
+
   return (
     <div className="login">
       <div className="sideA">
         <h2>Login</h2>
         <h3>WELCOME BACK!</h3>
-        <form>
+        <form onSubmit= { handleSubmit }>
           <label><span><MdPerson /></span>username </label>
-          <input type="text" />  <br /> <br />
+          <input type="text" value={ username } onChange = {(e) => {
+            setUsername(e.target.value) //sets the username to the value that is obtained
+          }}/>  
+          <br /> <br />
 
           <label><span><BiLockAlt /></span>password </label>
-          <input type="password" /> <br/>
-          
+          <input type="password" value={ password } onChange= {(e) => {
+            setPassword(e.target.value) //sets the password to the value that is obtained
+          }}/> 
+          <br/> 
+          <p><span className="errorMessage">{ error }</span></p>
+          <br/>
           <p className="loginForgotPassword">FORGOT PASSWORD</p> 
           <button type="submit">LOGIN &gt; </button>        
         </form>
